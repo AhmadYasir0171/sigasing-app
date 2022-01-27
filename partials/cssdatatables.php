17887cdcdf16ea6ce790de0cc87ba851608011f7
Ahmad Yasir <62274474+AhmadYasir0171@users.noreply.github.com>
2022-01-27 10:13:25 +0800

    Koneksi database dan datatables

---
+++ partials/cssdatatables.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="pluigins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="pluigins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="pluigins/datatables-buttons/css/button.bootstrap4.min.css">
